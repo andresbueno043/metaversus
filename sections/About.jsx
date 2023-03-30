2cd2bab6c50654bbcc7a0e8fb7deae11ab6edccd
andresbueno043 <ajbueno@iegabo.edu.co>
2023-03-30 16:03:37 -0500

    Hero section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { TypingText } from "../components";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
-const About = () => (
-  <section>
-    About section
-  </section>
-);
+const About = () => <section>About section</section>;
 
 export default About;
