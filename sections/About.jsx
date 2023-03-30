d3838b8c1e4ba219918be6838583b03674462296
andresbueno043 <ajbueno@iegabo.edu.co>
2023-03-30 16:18:48 -0500

    TypingText Component

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,14 @@
 "use client";
+
 import { motion } from "framer-motion";
 import styles from "../styles";
 import { TypingText } from "../components";
 import { fadeIn, staggerContainer } from "../utils/motion";
 
-const About = () => <section>About section</section>;
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    About section
+  </section>
+);
 
 export default About;
